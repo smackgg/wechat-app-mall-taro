256d5d2eab77e99f82651f9a0cd0abcb45e0d2c7
smackgg <smackgg@gmail.com>
2019-06-19 10:50:30 +0800

    test taro api

--- src/app.js
+++ src/app.js
@@ -4,9 +4,10 @@ import { Provider } from '@tarojs/redux'
 
 import Index from './pages/index'
 
-import configStore from './store'
+import configStore from './redux/store'
 
 import './app.less'
+import { UPDATE_GLOBAL_DATA } from './redux/actions/global'
 
 // 如果需要在 h5 环境中开启 React Devtools
 // 取消以下注释：
@@ -30,6 +31,59 @@ class App extends Component {
     }
   }
 
+  componentWillMount () {
+    // 检测版本更新
+    const updateManager = Taro.getUpdateManager()
+    updateManager.onUpdateReady(() => {
+      Taro.showModal({
+        title: '更新提示',
+        content: '新版本已经准备好，是否重启应用？',
+        success(res) {
+          if (res.confirm) {
+            // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+            updateManager.applyUpdate()
+          }
+        }
+      })
+    })
+
+    // 检测网络状况
+    Taro.getNetworkType({
+      success: (res) => {
+        const networkType = res.networkType
+        if (networkType === 'none') {
+          // 更新断网状态
+          this.updateNetworkStatus(false)
+          Taro.showToast({
+            title: '当前无网络',
+            icon: 'loading',
+            duration: 2000
+          })
+        }
+      }
+    })
+
+    // 监听网络状态
+    Taro.onNetworkStatusChange(res => {
+      if (!res.isConnected) {
+        // 更新断网状态
+        this.updateNetworkStatus(false)
+        Taro.showToast({
+          title: '网络已断开',
+          icon: 'loading',
+          duration: 2000,
+          complete: () => {
+            // 网络断开处理逻辑
+            // this.goStartIndexPage()
+          }
+        })
+      } else {
+        this.updateNetworkStatus(true)
+        Taro.hideToast()
+      }
+    });
+  }
+
   componentDidMount () {}
 
   componentDidShow () {}
@@ -37,6 +91,16 @@ class App extends Component {
   componentDidHide () {}
 
   componentDidCatchError () {}
+
+  // 更新网络状态
+  updateNetworkStatus = async isConnected => {
+    await store.dispatch({
+      type: UPDATE_GLOBAL_DATA,
+      data: {
+        isConnected
+      }
+    })
+  }
 
   // 在 App 类中的 render() 函数没有实际作用
   // 请勿修改此函数
