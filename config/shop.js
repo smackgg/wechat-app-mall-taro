9a80301c5808ac865969607dbaec6abcc68e5155
smackgg <smackgg@gmail.com>
2019-06-22 02:35:39 +0800

    fixed eslint

--- config/shop.js
+++ config/shop.js
@@ -3,5 +3,5 @@ module.exports = {
   note: '开票申请页面增加转发、可设置扩展属性',
   subDomain: "tz", // https://admin.it120.cc 登录后台首页的专属域名
   appid: "wxa46b09d413fbcaff", // 您的小程序的appid，购物单功能需要使用
-  shareProfile: '百款精品商品，总有一款适合您' // 首页转发的时候话术
+  shareProfile: '百款精品商品，总有一款适合您', // 首页转发的时候话术
 }
--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -2,7 +2,7 @@ import Taro, { Component } from '@tarojs/taro'
 import { View, Button, Text } from '@tarojs/components'
 import { connect } from '@tarojs/redux'
 
-import { add, minus, asyncAdd } from '../../actions/counter'
+import { add, minus, asyncAdd } from '@/redux/actions/counter'
 
 import './index.less'
 
