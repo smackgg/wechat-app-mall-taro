ba722c1f10abe05275449ff9a02ff5dec025c614
smackgg <smackgg@gmail.com>
2019-07-17 21:36:46 +0800

    update shopconfig

--- shopConfig.js
+++ shopConfig.js
@@ -6,19 +6,8 @@ module.exports = {
   shareProfile: '百款精品商品，总有一款适合您', // 首页转发的时候话术
   requireBindMobile: true, // 是否强制绑定手机号码才能使用
   theme: {
-    // '$color-brand': '#37313c',
-    // '$color-brand-light': '#69656d',
-    // '$color-brand-dark': '#2c2730',
     '$color-brand': '#37313c',
     '$color-brand-light': '#69656d',
     '$color-brand-dark': '#2c2730',
   }, // 主体颜色 详情见 https://nervjs.github.io/taro-ui-theme-preview/
 }
-
-/* Custom Theme */
-// $color-brand: #37313c;
-// $color-brand-light: #69656d;
-// $color-brand-dark: #2c2730;
-// '$color-brand': '#37313c',
-// '$color-brand-light': '#69656d',
-// '$color-brand-dark': '#2c2730',
