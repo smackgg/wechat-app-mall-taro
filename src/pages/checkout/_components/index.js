1b25f8af5e1a8383952f7794bf58c0b9b89cd560
smackgg <smackgg@gmail.com>
2019-07-25 19:13:57 +0800

    delete useless components

--- src/pages/checkout/_components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as PriceInfo } from './PriceInfo'
-export { default as ProductList } from './ProductList'
-export { default as Address } from './Address'
-export { default as BottomBar } from './BottomBar'
