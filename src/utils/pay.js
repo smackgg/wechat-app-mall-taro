65159f4d3e0292b0b80b24b4f210c3aaa946a0f3
smackgg <smackgg@gmail.com>
2019-07-25 18:54:10 +0800

    fixed pay bug

--- src/utils/pay.js
+++ src/utils/pay.js
@@ -83,7 +83,7 @@ export default function pay({
           } else {
             await wxPay({
               type,
-              money,
+              money: moneyReal,
               orderId,
             })
           }
