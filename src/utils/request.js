efb19e36ad9375a76238e98980fbcf7ad84de201
smackgg <smackgg@gmail.com>
2019-07-25 23:22:02 +0800

    fixed checktoken bug

--- src/utils/request.js
+++ src/utils/request.js
@@ -4,8 +4,16 @@ import { subDomain } from '@/utils'
 const API_BASE_URL = 'https://api.it120.cc'
 
 export default option => new Promise((resolve, reject) => {
-  const { url, needSubDomain = true } = option
+  const { url, needSubDomain = true, interceptTokenError = true, data = {} } = option
   let reqUrl = API_BASE_URL + (needSubDomain ? '/' + subDomain : '') + url
+
+  // 删减没有数据的参数
+  const requestData = Object.keys(data).reduce((pre, key) => {
+    if (data[key] !== undefined) {
+      pre[key] = data[key]
+    }
+    return pre
+  }, {})
 
   // 请求携带 token
   const token = Taro.getStorageSync('token')
@@ -15,13 +23,14 @@ export default option => new Promise((resolve, reject) => {
 
   Taro.request({
     ...option,
+    data: requestData,
     url: reqUrl,
     header: {
       'Content-Type': 'application/x-www-form-urlencoded',
     },
     success: res => {
       // token 失效
-      if (res && res.statusCode === 200 && res.data.code === 2000) {
+      if (res && res.statusCode === 200 && res.data.code === 2000 && interceptTokenError) {
         Taro.navigateTo({
           url: '/pages/authorize/index',
         })
