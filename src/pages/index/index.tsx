b156b5d7840b6386eb0d8964399b7c11164a83b5
smackgg <smackgg@gmail.com>
2019-09-16 14:51:54 +0800

    fixed tslint

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -7,12 +7,12 @@ import { add, minus, asyncAdd } from '@/redux/actions/counter'
 
 import './index.less'
 
-// #region ????
+// #region 书写注意
 //
-// ?? typescript ????????????? Props ??? Taro.Component ?? props ??
-// ?????? connect ???????? interface ????? Taro.Component ??? props
-// ??????????? IDE ?????
-// ???????????
+// 目前 typescript 版本还无法在装饰器模式下将 Props 注入到 Taro.Component 中的 props 属性
+// 需要显示声明 connect 的参数类型并通过 interface 的方式指定 Taro.Component 子类的 props
+// 这样才能完成类型检查和 IDE 的自动提示
+// 使用函数模式则无此限制
 // ref: https://github.com/DefinitelyTyped/DefinitelyTyped/issues/20796
 //
 // #endregion
@@ -22,8 +22,6 @@ type PageStateProps = {
     num: number
   }
 }
-
-var aaa = 1
 
 type PageDispatchProps = {
   add: () => void
@@ -38,73 +36,67 @@ type PageState = {}
 type IProps = PageStateProps & PageDispatchProps & PageOwnProps
 
 interface Index {
-  props: IProps;
+  props: IProps
 }
 
 @connect(({ counter }) => ({
-    counter,
+  counter,
 }), (dispatch) => ({
-    add () {
-        dispatch(add())
-    },
-    dec () {
-        dispatch(minus())
-    },
-    asyncAdd () {
-        dispatch(asyncAdd())
-    },
+  add() {
+    dispatch(add())
+  },
+  dec() {
+    dispatch(minus())
+  },
+  asyncAdd() {
+    dispatch(asyncAdd())
+  },
 }))
 class Index extends Component {
 
-    /**
-   * ??config??????: Taro.Config
-   *
-   * ?? typescript ?? object ???????? Key ?????
-   * ??? navigationBarTextStyle: 'black' ?????????? string
-   * ????? navigationBarTextStyle: 'black' | 'white' ????, ????????
-   */
-    config: Config = {
-        navigationBarTitleText: '??',
-    }
+  /**
+ * 指定config的类型声明为: Taro.Config
+ *
+ * 由于 typescript 对于 object 类型推导只能推出 Key 的基本类型
+ * 对于像 navigationBarTextStyle: 'black' 这样的推导出的类型是 string
+ * 提示和声明 navigationBarTextStyle: 'black' | 'white' 类型冲突, 需要显示声明类型
+ */
+  config: Config = {
+    navigationBarTitleText: '首页',
+  }
 
-    state = {
-        a: 1,
-    }
+  state = {
+    a: 1,
+  }
 
-    componentWillReceiveProps (nextProps) {
-        console.log(this.state.a)
-    }
+  componentWillReceiveProps(nextProps) {
+    console.log(this.state.a)
+  }
 
-    componentWillUnmount () { }
+  componentWillUnmount() { }
 
-    componentDidShow () {
-        // console.log(this.state.a);
-        this.fn('fds')
-    }
+  componentDidShow() {
+  }
 
-    componentDidHide () { }
+  componentDidHide() { }
 
-    public fn = (fsdf) => {
-        console.log('b: ' + fsdf)
-    }
-
-    render () {
-        return (
-            <View className="index">
-                <Button className="add_btn" onClick={this.props.add}>+</Button>
-                <Button className="dec_btn" onClick={this.props.dec}>-</Button>
-                <Button className="dec_btn" onClick={this.props.asyncAdd}>async</Button>
-                <View><Text>{this.props.counter.num}</Text></View>
-                <View><Text>Hello, World</Text></View>
-            </View>
-        )
-    }
+  render() {
+    return (
+      <View className="index">
+        <Button className="add_btn" onClick={this.props.add}>+</Button>
+        <Button className="dec_btn" onClick={this.props.dec}>-</Button>
+        <Button className="dec_btn" onClick={this.props.asyncAdd}>async</Button>
+        <View><Text>{this.props.counter.num}</Text></View>
+        <View><Text>Hello, World</Text></View>
+      </View>
+    )
+  }
 }
 
-// #region ????
+// #region 导出注意
 //
-// ?????????????? Taro.Component ?????????? props ??
-// ?????????? Ts ??????? JSX ??????
+// 经过上面的声明后需要将导出的 Taro.Component 子类修改为子类本身的 props 属性
+// 这样在使用这个子类时 Ts 才不会提示缺少 JSX 类型参数错误
 //
 // #endregion
 
