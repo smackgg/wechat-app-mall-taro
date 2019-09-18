eb145b909c875f1403b83f404a941f215dae5e55
smackgg <smackgg@gmail.com>
2019-09-18 16:21:25 +0800

    add pages

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,17 @@
 import '@tarojs/async-await'
 import Taro, { Component, Config } from '@tarojs/taro'
 import { Provider } from '@tarojs/redux'
+import { getVipLevel, getSystemConfig } from '@/redux/actions/config'
+import { checkToken } from '@/services/user'
+import { getUserDetail } from '@/redux/actions/user'
+import { requireBindMobile, showToast, ShowToastParam } from './utils'
 
 import Index from './pages/index'
-
 import { store } from './redux/store'
+import { UPDATE_GLOBAL_DATA } from './redux/actions/global'
 
-import './app.less'
+import './app.scss'
+
 
 // 如果需要在 h5 环境中开启 React Devtools
 // 取消以下注释：
@@ -24,33 +29,182 @@ class App extends Component {
    * 提示和声明 navigationBarTextStyle: 'black' | 'white' 类型冲突, 需要显示声明类型
    */
   config: Config = {
-      pages: [
-          'pages/index/index',
-      ],
-      window: {
-          backgroundTextStyle: 'light',
-          navigationBarBackgroundColor: '#fff',
-          navigationBarTitleText: 'WeChat',
-          navigationBarTextStyle: 'black',
-      },
+    pages: [
+        'pages/index/index',
+    ],
+    window: {
+      backgroundTextStyle: 'light',
+      navigationBarBackgroundColor: '#fff',
+      navigationBarTitleText: '',
+      navigationBarTextStyle: 'black',
+    },
   }
 
-  componentDidMount () {}
+  async componentWillMount() {
+    // 检测版本更新
+    const updateManager = Taro.getUpdateManager()
+    updateManager.onUpdateReady(() => {
+      Taro.showModal({
+        title: '更新提示',
+        content: '新版本已经准备好，是否重启应用？',
+        success(res) {
+          if (res.confirm) {
+            // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+            updateManager.applyUpdate()
+          }
+        },
+      })
+    })
 
-  componentDidShow () {}
+    // 检测网络状况
+    Taro.getNetworkType({
+      success: (res: { networkType: string }) => {
+        const networkType = res.networkType
+        if (networkType === 'none') {
+          // 更新断网状态
+          this.updateNetworkStatus(false)
+          showToast({
+            title: '当前无网络',
+            icon: 'loading',
+            duration: 2000,
+          })
+        }
+      },
+    })
 
-  componentDidHide () {}
+    // 监听网络状态
+    Taro.onNetworkStatusChange(res => {
+      if (!res.isConnected) {
+        // 更新断网状态
+        this.updateNetworkStatus(false)
+        showToast({
+          title: '网络已断开',
+          icon: 'loading',
+          duration: 2000,
+          complete: () => {
+            // 网络断开处理逻辑
+            // this.goStartIndexPage()
+          },
+        })
+      } else {
+        this.updateNetworkStatus(true)
+        Taro.hideToast()
+      }
+    })
+  }
 
-  componentDidCatchError () {}
+  async componentDidShow() {
+    // 获取 vipLevel
+    store.dispatch(getVipLevel())
 
+    // 获取系统参数（店铺信息等
+    store.dispatch(getSystemConfig({
+      keys: [
+        'index_video_1',
+        'index_video_2',
+        'recharge_amount_min',
+        'ALLOW_SELF_COLLECTION',
+        'concat_phone_number',
+        'mall_name',
+        'home_order_category_id',
+        'wifi_ssid',
+        'wifi_password',
+        'mall_avatar',
+      ].join(','),
+    }))
+
+    this.checkLogin().catch(() => {
+      // 未登录
+      // this.goToLoginPage()
+    })
+  }
+
+  checkLogin = (): Promise<any> => new Promise(async (resolve, reject) => {
+    try {
+      const token = Taro.getStorageSync('token')
+      // 本地没有登录 token
+      if (!token) {
+        return reject()
+      }
+
+      await this.checkSession()
+
+      // 校验 token 是否有效
+      const res = await checkToken()
+      // token 失效，清除本地 token 重新授权
+      if (res.code !== 0) {
+        Taro.removeStorageSync('token')
+        await this.showToastP({
+          title: '登录失效，请重新授权~',
+          icon: 'loading',
+          duration: 1000,
+        })
+        return reject()
+      }
+
+      // 获取用户详情
+      const userDetail = await store.dispatch(getUserDetail())
+
+      // 强制用户绑定手机号
+      if (requireBindMobile && !userDetail.mobile) {
+        await this.showToastP({
+          title: '需要授权并绑定手机号~',
+          icon: 'none',
+          duration: 2000,
+        })
+        return reject()
+      }
+
+      resolve()
+    } catch (e) {
+      reject(e)
+    }
+  })
+
+  // 检查用户授权的 session
+  checkSession = () => new Promise((resolve, reject) => {
+    Taro.checkSession({
+      success: resolve,
+      fail: reject,
+    })
+  })
+
+  showToastP = (options: ShowToastParam) => new Promise(resolve => {
+    showToast({
+      ...options,
+      complete: resolve,
+    })
+  })
+
+  componentDidHide() { }
+
+  componentDidCatchError() { }
+
+  // 更新网络状态
+  updateNetworkStatus = async (isConnected: boolean) => {
+    await store.dispatch({
+      type: UPDATE_GLOBAL_DATA,
+      data: {
+        isConnected,
+      },
+    })
+  }
+
+  goToLoginPage = () => {
+    // Taro.removeStorageSync('token')
+    setTimeout(() => {
+      Taro.navigateTo({
+        url: '/pages/authorize/index',
+      })
+    }, 300)
+  }
   // 在 App 类中的 render() 函数没有实际作用
   // 请勿修改此函数
   render () {
-    console.log(1)
       return (
-          <Provider store={store}>
-              <Index />
-          </Provider>
+        <Provider store={store}>
+          <Index />
+        </Provider>
       )
   }
 }
