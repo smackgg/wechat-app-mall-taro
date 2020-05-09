13117f3eed3f7c0a0d2061f089ac5c393ac41c08
smackgg <smackgg@gmail.com>
2020-05-09 18:34:38 +0800

    update app

---
+++ src/app.config.ts
@@ -0,0 +1,37 @@
+export default {
+  pages: [
+    'pages/index/index',
+    // 'pages/authorize/index',
+    // 'pages/account/index',
+    // 'pages/account/extinfo',
+    // 'pages/product-detail/index',
+    // 'pages/product-detail/share',
+    // 'pages/product-detail/reputations',
+    // // 'pages/checkout/index',
+    // 'pages/edit-address/index',
+    // 'pages/select-address/index',
+    // // 'pages/order-list/index',
+    // // 'pages/order-detail/index',
+    // 'pages/category/index',
+    // 'pages/asset/index',
+    // // 'pages/coupons/index',
+    // // 'pages/score-shop/index',
+    // // 'pages/reputation/index',
+    // 'pages/shop-cart/index',
+    // 'pages/recharge/index',
+    // 'pages/entry/index',
+    // 'pages/vip-center/index',
+    // 'pages/vip-center/my',
+    // 'pages/vip-center/potences',
+    // 'pages/contact/index',
+    // 'pages/wifi/index',
+    // 'pages/location/index'
+  ],
+  window: {
+    backgroundTextStyle: 'light',
+    navigationBarBackgroundColor: '#fff',
+    navigationBarTitleText: '',
+    navigationBarTextStyle: 'black'
+  },
+  navigateToMiniProgramAppIdList: ['wx60d176f873ca2d67', 'wxcefbc3693001e0d2']
+}
