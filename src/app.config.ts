5e0c55cfd9038a20053fac6aed4fe6b5a499e07b
smackgg <smackgg@gmail.com>
2020-05-12 20:04:42 +0800

    suport product share poster

--- src/app.config.ts
+++ src/app.config.ts
@@ -5,7 +5,7 @@ export default {
     'pages/account/index',
     // 'pages/account/extinfo',
     'pages/product-detail/index',
-    // 'pages/product-detail/share',
+    'pages/product-detail/share',
     // 'pages/product-detail/reputations',
     'pages/checkout/index',
     'pages/address-edit/index',
