ce86d11718418d9202f3070361317350a31cb46c
smackgg <smackgg@gmail.com>
2020-05-27 22:58:02 +0800

    add routes and router

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,31 +1,51 @@
+export const router = {
+  pages: {
+    index: 'index/index',
+    auth: 'authorize/index',
+    account: 'account/index',
+    category: 'category/index',
+    shopcart: 'shop-cart/index',
+  },
+  pages2: {
+    productDetail: 'product-detail/index',
+    productDetailShare: 'product-detail/share',
+    productDetailReputations: 'product-detail/reputations',
+    chekcout: 'checkout/index',
+    addressEdit: 'address-select/index',
+    addressSelect: 'address-edit/index',
+    orderList: 'order-list/index',
+    orderDetail: 'order-detail/index',
+    reputation: 'reputation/index',
+    asset: 'asset/index',
+    recharge: 'recharge/index',
+    coupons: 'coupons/index',
+    scoreShop: 'score-shop/index',
+    vipCenter: 'vip-center/index',
+    vipCenterMy: 'vip-center/my',
+    vipCenterPotences: 'vip-center/potences',
+  }
+}
+
+export const pages = Object.keys(router.pages).map(path => `pages/${router.pages[path]}`)
+
+type Routers = {
+  [key in (keyof typeof router['pages'])]: string
+} & {
+  [key in (keyof typeof router['pages2'])]: string
+}
+
+export const routes: Routers = Object.keys(router).reduce((r, packageName) => {
+  Object.keys(router[packageName]).forEach(routerName => {
+    r[routerName] = `${packageName}/${router[packageName][routerName]}`
+  })
+  return r
+}, {} as Routers)
+
 export default {
-  pages: [
-    'pages/index/index',
-    'pages/authorize/index',
-    'pages/account/index',
-    'pages/category/index',
-    'pages/shop-cart/index',
-  ],
+  pages,
   subPackages: [{
     root: 'pages2',
-    pages: [
-      'product-detail/index',
-      'product-detail/share',
-      'product-detail/reputations',
-      'checkout/index',
-      'address-select/index',
-      'address-edit/index',
-      'order-list/index',
-      'order-detail/index',
-      'reputation/index',
-      'asset/index',
-      'recharge/index',
-      'coupons/index',
-      'score-shop/index',
-      'vip-center/index',
-      'vip-center/my',
-      'vip-center/potences',
-    ],
+    pages: Object.values(router.pages2),
   }],
   tabBar: {
     color: '#333333',
@@ -34,25 +54,25 @@ export default {
     backgroundColor: '#fff',
     list: [
       {
-        pagePath: 'pages/index/index',
+        pagePath: routes.index,
         iconPath: 'assets/icon/home.jpg',
         selectedIconPath: 'assets/icon/home-selected.jpg',
         text: '商城首页',
       },
       {
-        pagePath: 'pages/category/index',
+        pagePath: routes.category,
         iconPath: 'assets/icon/category.jpg',
         selectedIconPath: 'assets/icon/category-selected.jpg',
         text: '分类',
       },
       {
-        pagePath: 'pages/shop-cart/index',
+        pagePath: routes.shopcart,
         iconPath: 'assets/icon/shopcart.jpg',
         selectedIconPath: 'assets/icon/shopcart-selected.jpg',
         text: '购物车',
       },
       {
-        pagePath: 'pages/account/index',
+        pagePath: routes.account,
         iconPath: 'assets/icon/account.jpg',
         selectedIconPath: 'assets/icon/account-selected.jpg',
         text: '我的',
@@ -64,5 +84,5 @@ export default {
     navigationBarBackgroundColor: '#fff',
     navigationBarTitleText: '',
     navigationBarTextStyle: 'black'
-  }
+  },
 }
