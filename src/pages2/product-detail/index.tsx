bb5cbd0850be5282ca4efd3bcacf69fb3ed1e1eb
smackgg <smackgg@gmail.com>
2020-05-28 11:15:24 +0800

    后生家订制预订需求

--- src/pages2/product-detail/index.tsx
+++ src/pages2/product-detail/index.tsx
@@ -237,6 +237,9 @@ export default class ProductDetail extends Component<PageProps, PageState> {
 
     const {
       pics,
+      basicInfo: {
+        tags,
+      },
       basicInfo,
       logistics,
       extJson,
@@ -245,6 +248,9 @@ export default class ProductDetail extends Component<PageProps, PageState> {
     const services = extJson.service
       ? extJson.service.split(' ')
       : null
+
+    // 是否为预订
+    const isReserve = !!(tags && tags.includes('在线定位'))
 
     // 评价
     const reputations = this.props.reputations[this.productId] || []
@@ -265,7 +271,7 @@ export default class ProductDetail extends Component<PageProps, PageState> {
         </Swiper>
 
         {/* 商品价格块 */}
-        <ProductPrice basicInfo={basicInfo} logistics={logistics} productId={this.productId}></ProductPrice>
+        <ProductPrice basicInfo={basicInfo} logistics={logistics} productId={this.productId} isReserve={isReserve}></ProductPrice>
 
         {/* 额外服务 */}
         {services && <View className="service">
@@ -327,20 +333,22 @@ export default class ProductDetail extends Component<PageProps, PageState> {
             </View>
             <View className="button-wrapper">
               <View className="add-card">
-                <Button
+                {!isReserve && <Button
                   className="button button-cart"
                   // full
                   type="primary"
                   onClick={this.showSelectSku.bind(this, 2)}
-                >加入购物车</Button>
+                >加入购物车</Button>}
               </View>
               <View>
                 <Button
-                  className={classNames('button button-buynow')}
+                  className={classNames('button button-buynow', {
+                    'is-reserve': isReserve,
+                  })}
                   // full
                   type="primary"
                   onClick={this.showSelectSku.bind(this, 1)}
-                >立即购买</Button>
+                >{isReserve ? '立即预订' : '立即购买'}</Button>
               </View>
             </View>
           </View>
@@ -355,6 +363,7 @@ export default class ProductDetail extends Component<PageProps, PageState> {
             handleClose={this.handleClose}
             buttonType={buttonType}
             addCart={this.props.addCart}
+            isReserve={isReserve}
           />
         </AtFloatLayout>
       </View>
